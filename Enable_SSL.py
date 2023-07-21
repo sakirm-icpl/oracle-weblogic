ad110a6450f19bf516c03b1076ff1fd82f8b4d55
sakirm-icpl <sakirm@infopercept.com>
2023-07-21 19:26:04 +0530

    added files

---
+++ Enable_SSL.py
@@ -0,0 +1,27 @@
+
+# Connect to WebLogic Admin Server
+connect('weblogic', 'P@$$w0rd', '172.17.11.58:7001')
+
+# Start an edit session
+edit()
+
+startEdit()
+
+# Navigate to the desired MBean location
+cd('Servers')
+cd('MS-1')
+cd('SSL')
+cd('MS-1')
+
+# Enable SSL
+cmo.setEnabled(true)
+
+# Save and activate the changes
+save()
+activate()
+
+# Disconnect from the WebLogic Admin Server
+disconnect()
+
+# Exit the script
+exit()
