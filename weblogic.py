f594f39f3bc45f591b279f1c11dcd6a513731b33
Sakir Malek <sakirm@infopercept.com>
2023-07-27 15:25:24 +0530

    Update weblogic.py

--- weblogic.py
+++ weblogic.py
@@ -1,56 +1,34 @@
-connect('weblogic', 'PocWLS0723$', '3.6.299.170:7001')
-
+connect('weblogic','PocWLS0723$','0.0.0.0:7001')
 edit()
-
 startEdit()
-
 cd('/Servers/MS1')
-cmo.setKeyStores('CustomIdentityAndCustomTrust')
-
-save()
-activate()
-
-startEdit()
 cmo.setCustomIdentityKeyStoreFileName('/home/sakir/certificate/Identity.jks')
 cmo.setCustomIdentityKeyStoreType('jks')
-setEncrypted('CustomIdentityKeyStorePassPhrase', 'CustomIdentityKeyStorePassPhrase_1690353949671', '/home/pavan/fmw/domains/poc_domain/Script1690353856071Config', '/home/pavan/fmw/domains/poc_domain/Script1690353856071Secret')
+setEncrypted('CustomIdentityKeyStorePassPhrase', 'CustomIdentityKeyStorePassPhrase_1690451358231', '/home/pavan/fmw/domains/poc_domain/Script1690451302889Config', '/home/pavan/fmw/domains/poc_domain/Script1690451302889Secret')
 cmo.setCustomTrustKeyStoreFileName('/home/sakir/certificate/Trust.jks')
 cmo.setCustomTrustKeyStoreType('jks')
-setEncrypted('CustomTrustKeyStorePassPhrase', 'CustomTrustKeyStorePassPhrase_1690353949749', '/home/pavan/fmw/domains/poc_domain/Script1690353856071Config', '/home/pavan/fmw/domains/poc_domain/Script1690353856071Secret')
-
+setEncrypted('CustomTrustKeyStorePassPhrase', 'CustomTrustKeyStorePassPhrase_1690451358275', '/home/pavan/fmw/domains/poc_domain/Script1690451302889Config', '/home/pavan/fmw/domains/poc_domain/Script1690451302889Secret')
 save()
 activate()
-
+edit()
 startEdit()
-
 cd('/Servers/MS1/SSL/MS1')
 cmo.setServerPrivateKeyAlias('new')
-setEncrypted('ServerPrivateKeyPassPhrase', 'ServerPrivateKeyPassPhrase_1690353991279', '/home/pavan/fmw/domains/poc_domain/Script1690353856071Config', '/home/pavan/fmw/domains/poc_domain/Script1690353856071Secret')
-
+setEncrypted('ServerPrivateKeyPassPhrase', 'ServerPrivateKeyPassPhrase_1690451382458', '/home/pavan/fmw/domains/poc_domain/Script1690451302889Config', '/home/pavan/fmw/domains/poc_domain/Script1690451302889Secret')
 save()
 activate()
-
+edit()
 startEdit()
-cmo.setExportKeyLifespan(500)
-cmo.setUseServerCerts(false)
-cmo.setSSLRejectionLoggingEnabled(true)
-cmo.setAllowUnencryptedNullCipher(false)
-cmo.setInboundCertificateValidation('BuiltinSSLValidationOnly')
-cmo.setOutboundCertificateValidation('BuiltinSSLValidationOnly')
 cmo.setHostnameVerificationIgnored(true)
 cmo.setHostnameVerifier(None)
 cmo.setTwoWaySSLEnabled(false)
 cmo.setClientCertificateEnforced(false)
-
 save()
 activate()
+edit()
 startEdit()
 cmo.setEnabled(true)
-
-save ()
+save()
 activate()
-
 disconnect()
-
 exit()
-
